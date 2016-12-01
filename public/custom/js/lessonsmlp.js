6ca9f1481390fde3265db73181b1890edec2e76e
slee333 <seungwooklee94@gmail.com>
2016-11-30 19:51:56 -0500

    recent update (2)

--- public/custom/js/lessonsmlp.js
+++ public/custom/js/lessonsmlp.js
@@ -1,7 +1,14 @@
+///////////////////////////////
+// 	       Initialize        //
+///////////////////////////////
+
+//D3.js unable to load data locally saved. Could import in javascript file.. Need to think about this.
+//Import and organize
 var korean_lessonsData = $.ajax({
 	url: "/api/lessonData",
 	type: "get",
 	success: function(){
+		showAbout()
 		console.log( " Data Loaded " );
 		var data_json = korean_lessonsData.responseJSON;
 		build_sublists('lesson-list', data_json)		
@@ -9,8 +16,13 @@ var korean_lessonsData = $.ajax({
 	}
 })
 
-// Layout *****
 
+
+
+
+
+
+// Layout *****
 var screen_height = $('.scroller-inner').height();
 var body_height = screen_height - $('.main-header').outerHeight();
 var margin = 60;
@@ -56,8 +68,6 @@ var build_sublists = function( divid, data ){
 				return title
 			})
 			.on('click', function(d, i ){
-				console.log( lessons[d].split('//') )
-				console.log( lessons[d].split('//').length )
 				removeLessons()
 				showLessons( lessons[d].split('//') )
 			})
@@ -136,6 +146,10 @@ var showLessons = function( sentences ) {
 	d3.select('.set-button').classed('button-selected', true)
 	showLessons(svg, group[0])
 
+	// Close menu.
+	d3.select('#mp-pusher').classed("mp-pushed", false)
+	d3.select('#mp-pusher').style("transform", 'translate3d(0,0,0)')
+
 	// Add sentence buttons
 	function showLessons(svg, sentences) {
 
@@ -172,8 +186,39 @@ var showLessons = function( sentences ) {
 
 	}
 
+}
 
+var showAbout = function( such_wow, a_mei_zing ) {
 
+	// Hard-coded Contents. Can separate these in another .js file, I guess.
+	var content = {
+		p1: "스피치바나나는 미국 존스홉킨스의 Ratnanather 교수팀이 만든 언어치료 및 청각재활 어플리케이션 입니다. 스피치바나나는 전문가와의 면담에 준하는 양질의 언어치료를 보다 쉽고 간편하게 제공하고자 하는 취지아래 개발되어 와우이식 착용자나 중등고도 이상의 난청으로 보청기를 착용하는 분들이 시간과 장소에 구애받지 않고 자율학습을 통해 언어습득 훈련이 되도록 하였습니다.",
+		p2: "본 스피치바나나 한국어판 개발은 영미권 사용자들을 위해 개발된 어플리케이션 기본틀에 우리말 특성을 고려한 단계적 학습이 될 수 있도록 우송대학교 언어청각재활학부 장선아 교수팀이 컨텐츠를 개발하고 분당서울대학교병원 이비인후과 구자원, 최병윤, 송재진 교수팀과 협업으로 이루어졌습니다. 또한 스피치바나나의 지문 및 레슨들은 본 어플리케이션의 제작취지에 공감한 MBC방송국 전문 아나운서들의 자발적인 참여로 녹음이 이루어져 이상적인 언어치료 보조도구로 완성되었습니다.",
+		p3: "여러분들의 참여와 피드백은 더 나은 어플리케이션을 제공하고자 하는 저희에게 큰 힘이 됩니다. 저희는 현재 사용자 분들의 의견을 설문이나 덧글을 통해 적극적으로 수용하며, 어플리케이션에 관한 질문이나 의견이 있다면  speechbanana.kr@cis.jhu.edu로 연락 주시거나 공식 홈페이지 (http://speechbananakorea.jhu.edu)를 방문하여 주시길 바랍니다.",
+		p4: "난청 환자들의 삶의 질을 향상 시키기 위한 프로젝트인 스피치 바나나를 이용 해 주셔서 깊이 감사 드리며, 사용자 분들의 소중한 의견을 반영하여 더욱 나은 서비스를 제공하도록 노력하겠습니다.",
+		help: "<b>도움 주신 분들:</b> <br><br>존스홉킨스대학: Tilak Ratnanather, 송조은, 임홍서, 이승욱, 에드릭 <br> 분당서울대학교병원: 구자원, 최병윤, 송재진, 김신혜, 이지혜, 한재준, 김다혜 <br> 우송대학교: 장선아, 정다혜, 전주희 <br>MBC 아나운서실: 김범도, 이재은, 차예린, 허일후, 윤홍일"
+	}
+
+	removeLessons()
+
+	var main_div = d3.select(".clearfix");
+	main_div.append("h1")
+		.text("스피치 바나나에 관하여")
+
+	var division = main_div.append('div')
+						.style('width', "100%")
+						.style('height', "4px")
+						.style('background-color', "#ffc526")
+
+	main_div.selectAll('p')
+		.data( Object.keys(content) ).enter()
+		.append('p')
+		.html( function(d, i){ return content[d] } )		// used .html instead of .text to use line break tag (<br>) included in the text
+		.attr('class', function(d,i){
+			if (d == 'help'){
+				return "about-help"
+			}
+		})
 }
 
 var removeLessons = function() {
@@ -184,3 +229,4 @@ var removeLessons = function() {
     	node.removeChild(node.lastChild);
 	}
 }
+
